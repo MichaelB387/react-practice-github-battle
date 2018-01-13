2f6fefd7f88bc0863e902c71a067e7f0410294ca
MichaelB387 <30610205+MichaelB387@users.noreply.github.com>
2018-01-13 14:53:51 +1100

    created component

---
+++ app/index.js
@@ -0,0 +1,16 @@
+var React = require('react');
+var ReactDom = require('react-dom');
+require('./index.css');
+
+class App extends React.Component {
+    // renders UI
+    render () {
+        return (
+            // This is JSX not HTML
+            <div>
+                Hello World!
+            </div>
+        )
+
+    }
+}
