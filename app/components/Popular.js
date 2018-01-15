03850e69baf4c6caa39bb9c9be628e7d7e02b9eb
MichaelB387 <30610205+MichaelB387@users.noreply.github.com>
2018-01-15 16:58:31 +1100

    Added state

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -2,20 +2,44 @@ var React = require('react');
 
 // This is ES6 syntax
 class Popular extends React.Component {
+    constructor(props){
+        super(props)
+        // INITIAL STATE FOR THIS COMPONENT
+        this.state = {
+            selectedLanguage: 'All'
+        }
+
+        this.updateLanguage = this.updateLanguage.bind(this)
+    }
+
+    updateLanguage(lang) {
+        // UPDATE STATE FOR THIS COMPONENT
+        this.setState(function(){
+            return {
+                selectedLanguage: lang
+            }
+        })
+    }
     // renders UI
     render () {
         var languages = ['All', 'Javascript', 'Ruby','Java', 'CSS', 'Python']
         return (
+            // UI FOR COMPONENT
             // This is JSX not HTML
+        <div>
             <ul className='languages'>
-                {languages.map(function(lang){
+                {languages.map((lang) => {
                     return (
-                        <li key={lang}>
+                        <li 
+                        style = {lang === this.state.selectedLanguage ? {color: '#d0021b'}: null}
+                        onClick={this.updateLanguage.bind(null,lang)}
+                        key={lang}>
                             {lang}
                         </li>
                     )
                 })}
             </ul>
+        </div>
         )
 
     }
